00d38711d774edf36901dd591e866e4bb3c87726
boris <boris.shaporov1@gmail.com>
2020-10-11 15:05:14 +0300

    added implementation of user details

--- src/main/java/com/security/university/entity/User.java
+++ src/main/java/com/security/university/entity/User.java
@@ -1,11 +1,15 @@
 package com.security.university.entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -19,6 +23,30 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+
     public Long getId() {
         return id;
     }
@@ -27,12 +55,13 @@ public class User {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
     }
 
     public String getPassword() {
