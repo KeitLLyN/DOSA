651a22e78a57dc054b3ac869c5b1757b174d3717
boris <boris.shaporov1@gmail.com>
2020-10-14 15:59:59 +0300

    Delete method for message

--- src/main/java/com/security/university/controllers/MainController.java
+++ src/main/java/com/security/university/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.security.university.controllers;
 
 import com.security.university.entity.Message;
+import com.security.university.entity.Role;
 import com.security.university.entity.User;
 import com.security.university.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +23,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+    public String main(@AuthenticationPrincipal User user,
+                       @RequestParam(required = false, defaultValue = "") String filter,
+                       Model model) {
         Iterable<Message> messages;
-
         if (filter != null && !filter.isEmpty()) {
             messages = MESSAGE_REPOSITORY.findByTag(filter);
         } else {
             messages = MESSAGE_REPOSITORY.findAll();
         }
-
         model.addAttribute("messages", messages);
         model.addAttribute("filter", filter);
-
+        model.addAttribute("isAdmin", user.getRoles().contains(Role.ADMIN));
         return "main";
     }
 
@@ -46,6 +47,16 @@ public class MainController {
         MESSAGE_REPOSITORY.save(message);
         Iterable<Message> messages = MESSAGE_REPOSITORY.findAll();
         model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam String message_id,
+                         @RequestParam String user_id,
+                         @AuthenticationPrincipal User user) {
+        if (user_id.equals(user.getId().toString()) || user.getRoles().contains(Role.ADMIN)) {
+            MESSAGE_REPOSITORY.deleteById(Long.parseLong(message_id));
+        }
         return "main";
     }
 }
