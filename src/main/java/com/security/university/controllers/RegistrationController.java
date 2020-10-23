9f633224a73045a2ffa80330850f64721b10ac75
boris <boris.shaporov1@gmail.com>
2020-10-23 15:49:02 +0300

    Added response from google recaptcha

--- src/main/java/com/security/university/controllers/RegistrationController.java
+++ src/main/java/com/security/university/controllers/RegistrationController.java
@@ -2,37 +2,57 @@ package com.security.university.controllers;
 
 import com.security.university.entity.Role;
 import com.security.university.entity.User;
+import com.security.university.entity.dto.CaptchaResponseDto;
 import com.security.university.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class RegistrationController {
+
     private final UserRepository USER_REPOSITORY;
     private final PasswordEncoder PASSWORD_ENCODER;
+    private final RestTemplate REST_TEMPLATE;
+
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
+    @Value("${recaptcha.secret}")
+    private String secret;
 
     @Autowired
-    public RegistrationController(UserRepository USER_REPOSITORY, PasswordEncoder PASSWORD_ENCODER) {
-        this.USER_REPOSITORY = USER_REPOSITORY;
-        this.PASSWORD_ENCODER = PASSWORD_ENCODER;
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder, RestTemplate restTemplate) {
+        this.USER_REPOSITORY = userRepository;
+        this.PASSWORD_ENCODER = passwordEncoder;
+        this.REST_TEMPLATE = restTemplate;
     }
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model){
+    public String addUser(@RequestParam("g-recaptcha-response") String captchaResponse,
+                          User user,
+                          Model model) {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = REST_TEMPLATE.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+        if (response != null && !response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill the captcha");
+            return "registration";
+        }
         User userFromDB = USER_REPOSITORY.findByUsername(user.getUsername());
-        if (userFromDB != null){
-            model.put("isExist",true);
+        if (userFromDB != null) {
+            model.addAttribute("isExist", true);
             return "registration";
         }
 
