5d9145ef55088e25c82144ce2437127d1dc3d98f
boris <boris.shaporov1@gmail.com>
2020-10-23 22:35:45 +0300

    Added property source path for login and register forms

--- src/main/java/com/security/university/config/MvcConfig.java
+++ src/main/java/com/security/university/config/MvcConfig.java
@@ -8,11 +8,13 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@PropertySource("classpath:login.properties")
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
