3280b67ea3d46dcd67a517a3ed63d7a90c4d2292
boris <boris.shaporov1@gmail.com>
2020-10-23 16:15:28 +0300

    Bean Rest Template

--- src/main/java/com/security/university/config/MvcConfig.java
+++ src/main/java/com/security/university/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.security.university.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,5 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
