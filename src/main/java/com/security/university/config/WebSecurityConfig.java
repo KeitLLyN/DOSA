19def5bc09712c84d2c4c6ec225b391776a1bea3
boris <boris.shaporov1@gmail.com>
2020-10-30 17:43:59 +0300

    Added /activate/* page for all users

--- src/main/java/com/security/university/config/WebSecurityConfig.java
+++ src/main/java/com/security/university/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         if (requireHttps){
             http
                     .authorizeRequests()
-                    .antMatchers("/", "/registration").permitAll()
+                    .antMatchers("/", "/registration", "/activate/*").permitAll()
                     .anyRequest().authenticated()
                     .and()
                         .formLogin()
