0e003f80c4234dadde4fe0957c9249df1bb59a97
boris <boris.shaporov1@gmail.com>
2020-10-30 17:41:52 +0300

    Password encoder as a field

--- src/main/java/com/security/university/config/WebSecurityConfig.java
+++ src/main/java/com/security/university/config/WebSecurityConfig.java
@@ -3,15 +3,12 @@ package com.security.university.config;
 import com.security.university.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -20,13 +17,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService USER_SERVICE;
+    private final PasswordEncoder PASSWORD_ENCODER;
 
     @Value("${security.require-ssl}")
     private boolean requireHttps;
 
     @Autowired
-    public WebSecurityConfig(UserService userService) {
+    public WebSecurityConfig(UserService userService, PasswordEncoder passwordEncoder) {
         this.USER_SERVICE = userService;
+        this.PASSWORD_ENCODER = passwordEncoder;
     }
 
 
@@ -54,11 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(USER_SERVICE)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                .passwordEncoder(PASSWORD_ENCODER);
     }
 }
